47f908f7b05e352d6da2b8ae592234a206c84605
SwapnilVarkhade <150380931+SwapnilVarkhade@users.noreply.github.com>
2024-03-18 17:02:25 +0530

    added succesfully

--- src/test/java/basic1/OrganizationTest.java
+++ src/test/java/basic1/OrganizationTest.java
@@ -1,5 +1,22 @@
 package basic1;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class OrganizationTest {
+	
+
+	
+
+	public class OrgaizationTest {
+	     
+		@Test
+		public void CreateOrganizationsTest() {
+			
+			
+			 WebDriver driver=new ChromeDriver();
+			 driver.get("http://localhost:8888/");
+		}
+	}
 
 }
